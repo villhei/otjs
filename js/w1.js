00a204316b9e4e5b95e61d8efb40f14c49d78c01
Ville Heikkinen <ville.heikkinen@gmail.com>
2013-11-09 00:00:47 +0200

    initial commit, unit tests for W1 integer checking

---
+++ js/w1.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Ville
+ * Date: 8.11.2013
+ * Time: 23:47
+ * INSERT W1 TypeCheck -class or something
+ */
+
+
+var Type = {
+
+    integer: (function() {
+
+    })
+
+};
---
+++ js/w1tests.js
@@ -0,0 +1,18 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Ville
+ * Date: 10.9.2013
+ * Time: 15:55
+ * To change this template use File | Settings | File Templates.
+ */
+
+test("Integer tests", function () {
+
+    ok(Type.integer(1), "Should determine 1 is an integer");
+    ok(Type.integer(0), "Should determine 0 is an integer");
+    equal(Type.integer(1.1), false, "Should determine 1.1 is not an integer");
+    equal(Type.integer(1.0001), false, "Should determine 1.1 is not an integer");
+    equal(Type.integer("1"), false, "Should determine string '1' is not an integer");
+    equal(Type.integer(null), false, "Should determine string null is not an integer");
+    equal(Type.integer([]), false, "Should determine string an array is not an integer");
+});
