88eed7d50dee85a428224c31ad925c1a2776c8d9
Ville Heikkinen <ville.heikkinen@gmail.com>
2013-11-11 23:09:25 +0200

    refactored some neater code

--- js/w1.js
+++ js/w1.js
@@ -9,57 +9,61 @@
 
 var Type = {
 
-    integer:function (input) {
-        if (!this.number(input)) {
-            return false;
-        }
-        return input % 1 === 0;
-    },
-
-    number:function (input) {
-        return this._typeOfTest('number', input);
-    },
-
-    string:function (input) {
-        return this._typeOfTest('string', input);
-    },
-
-    boolean:function (input) {
-        return this._typeOfTest('boolean', input);
-    },
-
-    intArray:function (input) {
-        var _this = this;
-        return this.arrayContains(
-            (function (testValue) {
-                return _this.integer(testValue);
-            }), input);
-    },
-
-    numberArray:function (input) {
-        var _this = this;
-        return this.arrayContains(
-            (function (testValue) {
-                return _this.number(testValue);
-            }), input);
-    },
-
-    arrayContains:function (testFunction, inputArray) {
-
-        // first we need to check if input really is an array
-        if (Object.prototype.toString.call(inputArray) !== '[object Array]') {
-            return false;
-        }
-        for (var i = 0, len = inputArray.length; i < len; i++) {
-            if (!testFunction(inputArray[i])) {
+        integer:function (input) {
+            if (!this.number(input)) {
                 return false;
             }
+            return input % 1 === 0;
+        },
+
+        number:function (input) {
+            return this._typeOfTest('number', input);
+        },
+
+        string:function (input) {
+            return this._typeOfTest('string', input);
+        },
+
+        boolean:function (input) {
+            return this._typeOfTest('boolean', input);
+        },
+
+        intArray:function (input) {
+            var _this = this;
+            return this.arrayContains(
+                (function (testValue) {
+                    return _this.integer(testValue);
+                }), input);
+        },
+
+        numberArray:function (input) {
+            var _this = this;
+            return this.arrayContains(
+                (function (testValue) {
+                    return _this.number(testValue);
+                }), input);
+        },
+
+        arrayContains:function (testFunction, inputArray) {
+
+            // first we need to check if input really is an array
+            if (Object.prototype.toString.call(inputArray) !== '[object Array]') {
+                return false;
+            }
+            this._testArrayElementsWith(testFunction, inputArray);
+        },
+
+        _testArrayElementsWith:function (testFunction, inputArray) {
+            for (var i = 0, len = inputArray.length; i < len; i++) {
+                if (!testFunction(inputArray[i])) {
+                    return false;
+                }
+            }
+            return true;
+        },
+
+        _typeOfTest:function (type, input) {
+            return typeof input === type;
         }
-        return true;
-    },
 
-    _typeOfTest:function (type, input) {
-        return typeof input === type;
-    }
-
-};
+    };
