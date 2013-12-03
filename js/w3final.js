629b19c26d9f357185fa7945f5b476a4ea58c349
Jami Karvanen <jami@karvanen.fi>
2013-12-03 11:15:18 +0200

    Add some stuff about prototype-inheritance

--- js/w3final.js
+++ js/w3final.js
@@ -1,4 +1,22 @@
 /*
+ Olion omien kenttien läpikäynnissä tulee olla tarkkana. Seuraava triviaali toteutus johtaa odottamattomiin seurauksiin
+*/
+for (var key in myObject) {
+    console.log(myObject[key])
+}
+
+/*
+ Mikäli tarkoituksena on käydä läpi vain kyseisen olion _omat_ kentät, jättäen väliin kaikki prototyyppiketjusta perityt,
+ onnistuu se seuraavasti
+ */
+for (var key in myObject) {
+    if (myObject.hasOwnProperty(key)) {
+        console.log(myObject[key])
+    }
+}
+
+
+/*
 Singleton-mallissa perinteinen literaalitoteutus on hyväksyttävä tapa. 
 */
 function SingletonObject() {
@@ -20,7 +38,7 @@ function Product(material, cost) {
    
          this.material = material;
          this.cost = cost;
-         this.totalCost = function() { this.material*this.cost };
+         this.totalCost = function() { return this.material*this.cost };
 }
    
    var product = new Product(100, 50);
